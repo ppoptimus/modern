c4a680e83de2e2420990754d7e46d7cee5fd72ea
rpumpo <r.pumpo@gmail.com>
2023-01-23 16:53:11 +0700

    start save data

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -106,7 +106,7 @@ const Header = ({ onChangeLayoutMode, layoutModeType, headerClass }) => {
                                     <Form className="p-3">
                                         <div className="form-group m-0">
                                             <div className="input-group">
-                                                <input type="text" className="form-control" placeholder="Search ..."
+                                                <input type="text" className="form-control" placeholder="SearchZ ..."
                                                     aria-label="Recipient's username" />
                                                 <button className="btn btn-primary" type="submit"><i
                                                     className="mdi mdi-magnify"></i></button>
